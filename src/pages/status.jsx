7ca198ebf5273e8eaba10786ce44e5e8c33fdc61
koichi <111332153+koichi2426@users.noreply.github.com>
2024-05-26 21:10:55 +0900

    ルーティング
    認証するページを用意するためにルーティング

--- src/pages/status.jsx
+++ src/pages/status.jsx
@@ -1,1 +1,12 @@
-status
+import React from 'react';
+
+const Status = () => {
+  return (
+    <div>
+      <h1>Status Page</h1>
+      <p>This is the status page.</p>
+    </div>
+  );
+};
+
+export default Status;
